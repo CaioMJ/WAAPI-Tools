054890ddfc4c0df496dd579c73c2573ea33161fb
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2023-09-18 16:02:27 -0400

    Update Set All Music Tracks To Stream.py

--- Scripts/Set All Music Tracks To Stream.py
+++ Scripts/Set All Music Tracks To Stream.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-
-import json
+import os
 from waapi import WaapiClient, CannotConnectToWaapiException
 from pprint import pprint
 
+#####FUNCTIONS#######
 def getAllMusicTrackIDs():
-    get_args = {
+    args = {
         "waql": "$ from type MusicTrack"
     }
 
-    get_option = {
+    options = {
         "return":["id"]
     }
 
-    return client.call("ak.wwise.core.object.get", get_args, options=get_option)
+    return client.call("ak.wwise.core.object.get", args, options=options)
         
 def getNumberOfObjects(result_dictionary):
     num_of_objects = 0
@@ -30,7 +30,7 @@ def setMusicTrackSettings(num_of_objects, result_dictionary, zerolatency, noncac
     while (count < num_of_objects):
         object_id = result_dictionary.get("return")[count]["id"]
         
-        set_args = {
+        args = {
             "objects":[
                 {
                     #Modifies an existing object
@@ -41,29 +41,25 @@ def setMusicTrackSettings(num_of_objects, result_dictionary, zerolatency, noncac
                 }
             ]
         }
-        client.call("ak.wwise.core.object.set", set_args)
-        pprint(count)
+        client.call("ak.wwise.core.object.set", args)
         count = count + 1
-        
+    else:
+        displaySystemMessage("Wwise music streaming configuration set!", "WAAPI Message")
+
+def displaySystemMessage(body_Str, title_Str):
+        os.system("osascript -e \'Tell application \"System Events\" to display dialog \""+body_Str+"\" with title \""+title_Str+"\"\'")
+
+######EXECUTION#####
 try:
     # Connecting to Waapi using default URL
     with WaapiClient() as client:
-        #User settings
         input_zerolatency = input("Zero Latency? True or False: ")
-        input_noncache = input("Is it non-cacheable? True or False: ")
+        input_noncache = input("Non-cacheable? True or False: ")
         
-        #Query to get all music track object ids
         get_result = getAllMusicTrackIDs()
-        pprint(get_result)
-        wait = input("Wait")
-        
-        #Get size of result list
         num_of_objects = getNumberOfObjects(get_result)
-        wait = input("Wait")
-
-        #Iterate through every object to set properties
+        pprint("Number of Music Tracks altered: " + str(num_of_objects))
         setMusicTrackSettings(num_of_objects, get_result, input_zerolatency, input_noncache)
 
-            
 except CannotConnectToWaapiException:
     print("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")

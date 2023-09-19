8dd4705f3af13ba6e795d8e2be1b2977b78b5c87
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2023-09-18 20:25:53 -0400

    pre property set refactoring

--- Wwise Auto Set Music Streaming Settings.py
+++ Wwise Auto Set Music Streaming Settings.py
@@ -5,72 +5,96 @@ from pprint import pprint
 
 #####FUNCTIONS#######
 def getAllMusicTrackIDs():
-    args = {
-        "waql": "$ from type MusicTrack"
-    }
+    #if path is empty, query the whole project for Music Tracks
+    if(input_path == ""):
+        args = {
+            "waql": "$ from type MusicTrack"
+        }
 
-    options = {
-        "return":["id"]
-    }
+        options = {
+            "return":["id", "type"]
+        }
 
-    return client.call("ak.wwise.core.object.get", args, options=options)
-        
-def getNumberOfObjects(result_dictionary):
+        return client.call("ak.wwise.core.object.get", args, options=options)
+    #if parent path is specified, query only the descendants of that object
+    else:
+        args = {
+            "waql": """$ "{}" select descendants""".format(input_path)
+        }
+
+        options = {
+            "return":["id", "type"]
+        }
+
+        return client.call("ak.wwise.core.object.get", args, options=options)
+                
+def getNumberOfMusicTracks(result_dictionary):
     num_of_objects = 0
-    
+    #gets total number of objects queried
     for x in result_dictionary.values():
         for i in x: num_of_objects += 1
         
-    pprint(num_of_objects)
     return num_of_objects
     
 def setMusicTrackSettings(num_of_objects, result_dictionary, zerolatency, noncache):
     count = 0
     
     while (count < num_of_objects):
-        object_id = result_dictionary.get("return")[count]["id"]
+        #if it's a music track, set properties
+        if (result_dictionary.get("return")[count]["type"] == "MusicTrack"):
         
-        #streaming
-        args = {
-            "object":object_id,
-            "property":"IsStreamingEnabled",
-            "value":True
-        }
-        
-        client.call("ak.wwise.core.object.setProperty", args)
-        
-        #zero latency
-        args = {
-            "object":object_id,
-            "property":"IsZeroLantency",
-            "value":zerolatency
-        }
-        
-        client.call("ak.wwise.core.object.setProperty", args)
-        
-        #non cache
-        args = {
-            "object":object_id,
-            "property":"IsNonCachable",
-            "value":noncache
-        }
-        
-        client.call("ak.wwise.core.object.setProperty", args)
+            object_id = result_dictionary.get("return")[count]["id"]
+            #streaming
+            args = {
+                "object":object_id,
+                "property":"IsStreamingEnabled",
+                "value":True
+            }
+            
+            client.call("ak.wwise.core.object.setProperty", args)
+            
+            #zero latency
+            args = {
+                "object":object_id,
+                "property":"IsZeroLatency",
+                "value":zerolatency
+            }
+            
+            client.call("ak.wwise.core.object.setProperty", args)
+            
+            #non cache
+            args = {
+                "object":object_id,
+                "property":"IsNonCachable",
+                "value":noncache
+            }
+            
+            client.call("ak.wwise.core.object.setProperty", args)
+            
         count = count + 1
+    else:
+        pprint("Music streaming configuration done!")
 
-def displaySystemMessage(body_Str, title_Str):
-        os.system("osascript -e \'Tell application \"System Events\" to display dialog \""+body_Str+"\" with title \""+title_Str+"\"\'")
+#def setProperty(object_id, propertyToSet, valueToSet):
+
+
 
 ######EXECUTION#####
 try:
     # Connecting to Waapi using default URL
     with WaapiClient() as client:
+        input_path = input("Music Hierarchy path (or leave blank): ")
         input_zerolatency = input("Zero Latency? True or False: ")
         input_noncache = input("Non-cacheable? True or False: ")
         
+
+        #pprint("Setting streaming configuration...")
         get_result = getAllMusicTrackIDs()
-        num_of_objects = getNumberOfObjects(get_result)
-        pprint("Number of Music Tracks altered: " + str(num_of_objects))
+        #pprint(get_result)
+        
+        num_of_objects = getNumberOfMusicTracks(get_result)
+
+        #input("STOP")
         setMusicTrackSettings(num_of_objects, get_result, input_zerolatency, input_noncache)
 
 except CannotConnectToWaapiException:
